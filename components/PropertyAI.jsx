6db0f03fb5d68c52f09dc291dc3a09f9574f1577
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:35:35 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for Alaknanda 
 
 Firm facts:
 - Firm: Alaknanda Enterprises
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: SCO 110, Phase 11 / Sector 67 Commercial Market, Mohali, Punjab 160062
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
